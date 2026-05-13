a582826884da3bb57ba06cb4f591f4ceb81a5122
João Antônio <joao.antonio05@outlook.com>
2026-05-13 14:39:06 -0300

    feat: primeira versão do código do sensor DHT22, para uma ESP8266

---
+++ firmware_irrigation/src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN D2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("DHT22 test!");
+
+  pinMode(DHTPIN, INPUT_PULLUP);
+  dht.begin();
+}
+
+void loop() {
+  delay(5000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Umidade: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.print(" *C\t");
+  Serial.print("Sensação Térmica: ");
+  Serial.print(hic);
+  Serial.println(" *C");
+}
