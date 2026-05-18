12cc81e1234046c838fa3c0a442261902c8704c4
João Antônio <joao.antonio05@outlook.com>
2026-05-18 12:35:12 -0300

    feat: login de test com dados mockados

--- projeto_integrador_backend/app/database.py
+++ projeto_integrador_backend/app/database.py
@@ -1,6 +1,14 @@
-# Simulação de um banco de dados em memória
+from app.schemas import Usuario, DadosUsuario
 
-db_usuarios = {}
-db_dados_usuarios = {}
+# Simulação de um banco de dados em memória com um usuário padrão
+
+db_usuarios = {
+    "testuser": Usuario(id=1, nome_usuario="testuser", senha="password")
+}
+
+db_dados_usuarios = {
+    1: DadosUsuario(id=1, email="test@user.com", fk_id_usuario=1)
+}
+
 db_brokers = {}
 db_cadastros_irrigacao = {}
